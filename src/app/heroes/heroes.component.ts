adbb8d373b295f6aa18055b9af1ac47696b8662d
Walter Bernal <wltrbm@gmail.com>
2023-10-21 11:54:39 -0700

    In Heroes Component: addition of messageService at constructor, and addition of message at selection of Hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,41 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { Hero } from '../hero'
-import { HeroService } from '../hero.service';
+import { HeroService } from '../hero.service'
+import { MessageService } from '../message.service'
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent {
-  constructor(private heroService: HeroService) {
+export class HeroesComponent implements OnInit {
+  selectedHero?: Hero;
+  heroes: Hero[] = [];
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {
     // The parameter simultaneously defines a private heroService property and identifies it as a HeroService injection site.
     /*
     Reserve the constructor for minimal initialization such as wiring constructor parameters to properties. The constructor shouldn't do anything. It certainly shouldn't call a function that makes HTTP requests to a remote server as a real data service would.
     */
   }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
   getHeroes(): void {
     // this.heroes = this.heroService.getHeroes()
-    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes)); // Fix to work with Observable class
+
+    // Fix to work with Observable class
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
 
     /*
     Code asynchronously awaits for the Observable to emit the array of heroes, 
@@ -24,18 +43,5 @@ export class HeroesComponent {
     The subscribe() method passes the emitted array to the callback, 
     which sets the component's heroes property
     */
-  }
-  ngOnInit(): void {
-    this.getHeroes()
-  }
-
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  }
-  heroes: Hero[] = []
-  selectedHero?: Hero
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero
   }
 }
