337fdbb5e1df2fb37f41041e5932da2b56996eab
Walter Bernal <wltrbm@gmail.com>
2023-10-21 11:53:24 -0700

    addition of messageService at constructor, and addition of message at call of getHeroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,17 +2,28 @@ import { Injectable } from '@angular/core'
 import { Hero } from './hero'
 import { HEROES } from './mock-heroes'
 import { Observable, of } from 'rxjs'
+import { MessageService } from './message.service'
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor() {}
+  constructor(private messageService: MessageService) {
+    /* 
+    Angular injects the singleton MessageService into that property when it creates the HeroService.
+    This is an example of a typical service-in-service scenario in which you inject 
+    the MessageService into the HeroService which is injected into the HeroesComponent.
+    */
+  }
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
     /* of(HEROES) returns an Observable<Hero[]> that emits a single value, 
     the array of mock heroes.*/
     // This allows to work asynchronously
-    return heroes;
+    const heroes = of(HEROES)
+
+    //send a message when the heroes are fetched
+    this.messageService.add('HeroService: fetched heroes')
+
+    return heroes
   }
 }
