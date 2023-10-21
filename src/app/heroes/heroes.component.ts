cd2aa789e31285955cd31af05e75e913056834e4
Walter Bernal <wltrbm@gmail.com>
2023-10-21 11:17:08 -0700

    @ Heroes component: Addition of constructor with a private heroService property so as its identification as injectino site, addition of getHeores method to define heroes property to heroService.getHeroes method, and call in ngOnInit to call the method getHeroes of Heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,33 @@
 import { Component } from '@angular/core'
 import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
+  constructor(private heroService: HeroService) {
+    // The parameter simultaneously defines a private heroService property and identifies it as a HeroService injection site.
+    /*
+    Reserve the constructor for minimal initialization such as wiring constructor parameters to properties. The constructor shouldn't do anything. It certainly shouldn't call a function that makes HTTP requests to a remote server as a real data service would.
+    */
+  }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  
   hero: Hero = {
     id: 1,
-    name: 'Windstorm'
-  }
-  heroes = HEROES
-  selectedHero?: Hero
+    name: 'Windstorm',
+  };
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
   onSelect(hero: Hero): void {
-    this.selectedHero = hero
+    this.selectedHero = hero;
   }
 }
